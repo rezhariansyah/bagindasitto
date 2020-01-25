2b3214d0b85b45689d4a01bfde43f22d7c5342cf
rezhariansyah <rezharatulangi@gmail.com>
2020-01-25 20:16:19 +0700

    jumbotron update

---
+++ src/screens/comingSoon/ComingSoon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ComingSoon.css'
+
+const ComingSoon = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ComingSoon;
