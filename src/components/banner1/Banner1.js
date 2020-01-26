7fe0593ede99482df6181f063eccbb9c6cf5343e
rezhariansyah <rezharatulangi@gmail.com>
2020-01-26 17:07:26 +0700

    add another banner

---
+++ src/components/banner1/Banner1.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+import './Banner1.css'
+
+const Banner1 = () => {
+    return (
+        <Fragment>
+            <div
+                className="jumbotron jumbotron-fluid banner mt-4"
+            >
+            </div>
+        </Fragment>
+    )
+}
+
+export default Banner1
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -2,6 +2,8 @@ import React, { Component, Fragment } from 'react';
 import Banner from '../../components/banner/Banner';
 import Categories from '../../components/categories/Categories';
 import LatestArticles from '../../components/latestArticles/LatestArticles';
+import Banner1 from '../../components/banner1/Banner1';
+
 
 class Home extends Component {
     render() {
@@ -10,6 +12,7 @@ class Home extends Component {
                 <Banner />
                 <Categories />
                 <LatestArticles />
+                <Banner1 />
             </Fragment>
         );
     }
