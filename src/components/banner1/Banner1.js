82a5d5383cf13264b7e743932cffb99a0cb503f9
rezhariansyah <rezharatulangi@gmail.com>
2020-04-15 03:34:09 +0800

    layouting update

--- src/components/banner1/Banner1.js
+++ src/components/banner1/Banner1.js
@@ -1,16 +1,85 @@
-import React, { Fragment } from 'react'
+import React, { useState } from "react";
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption,
+} from "reactstrap";
+import about from "../../assets/banners/carousel.jpg";
+import travel from "../../assets/banners/carousel2.jpg";
+import phinisi from "../../assets/banners/compress/banner3-min.jpg";
 import './Banner1.css'
 
-const Banner1 = () => {
-    return (
-        <Fragment>
-            <div
-                className="jumbotron jumbotron-fluid banner mt-4"
-            >
-            </div>
-            
-        </Fragment>
-    )
-}
+const items = [
+  {
+    src: about,
+  },
+  {
+    src: travel,
+  },
+  {
+    src: phinisi,
+  },
+];
 
-export default Banner1
+const Banner1 = (props) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  };
+
+  const slides = items.map((item) => {
+    return (
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.src}
+      >
+        <img src={item.src} alt={item.altText} />
+        <CarouselCaption
+          captionText={item.caption}
+          captionHeader={item.caption}
+        />
+      </CarouselItem>
+    );
+  });
+
+  return (
+    <Carousel activeIndex={activeIndex} next={next} previous={previous} className="carousel-fade">
+      <CarouselIndicators
+        items={items}
+        activeIndex={activeIndex}
+        onClickHandler={goToIndex}
+      />
+      {slides}
+      <CarouselControl
+        direction="prev"
+        directionText="Previous"
+        onClickHandler={previous}
+      />
+      <CarouselControl
+        direction="next"
+        directionText="Next"
+        onClickHandler={next}
+      />
+    </Carousel>
+  );
+};
+
+export default Banner1;
--- src/components/galleries/Galleries.js
+++ src/components/galleries/Galleries.js
@@ -1,6 +1,5 @@
 import React, { Component, Fragment } from "react";
 import Gallery from "react-grid-gallery";
-import { Fade } from "react-awesome-reveal";
 import image1 from "../../assets/banners/galleries/DSC02532 (1)-min.jpg";
 import image2 from "../../assets/banners/galleries/DSC04061-min.JPG";
 import image3 from "../../assets/banners/galleries/DSC02564-min.jpg";
@@ -8,50 +7,51 @@ import image4 from "../../assets/banners/galleries/DSC04522-min.JPG";
 import image5 from "../../assets/banners/galleries/GOPR2397-min.JPG";
 import image6 from "../../assets/banners/galleries/IMG_0461-min.jpg";
 import image7 from "../../assets/banners/galleries/IMG_3178-min.JPG";
+import "./Galleries.css";
 
 const images = [
   {
     src: image1,
     thumbnail: image1,
     thumbnailWidth: 380,
-    thumbnailHeight: 412
+    thumbnailHeight: 412,
   },
   {
     src: image2,
     thumbnail: image2,
     thumbnailWidth: 380,
-    thumbnailHeight: 212
+    thumbnailHeight: 212,
   },
   {
     src: image3,
     thumbnail: image3,
     thumbnailWidth: 380,
-    thumbnailHeight: 212
+    thumbnailHeight: 212,
   },
   {
     src: image4,
     thumbnail: image4,
     thumbnailWidth: 380,
-    thumbnailHeight: 412
+    thumbnailHeight: 412,
   },
   {
     src: image5,
     thumbnail: image5,
     thumbnailWidth: 380,
-    thumbnailHeight: 212
+    thumbnailHeight: 212,
   },
   {
     src: image6,
     thumbnail: image6,
     thumbnailWidth: 380,
-    thumbnailHeight: 212
+    thumbnailHeight: 212,
   },
   {
     src: image7,
     thumbnail: image7,
     thumbnailWidth: 380,
-    thumbnailHeight: 212
-  }
+    thumbnailHeight: 212,
+  },
 ];
 
 class Galeries extends Component {
@@ -62,7 +62,7 @@ class Galeries extends Component {
       backgroundPosition: "center",
       objectFit: "cover",
       height: "100%",
-      cursor: "pointer"
+      cursor: "pointer",
     };
   }
 
@@ -70,22 +70,60 @@ class Galeries extends Component {
     return (
       <Fragment>
         <div className="container">
-          <Fade delay={300}>
-            <div className="row justify-content-center mt-5 mb-5">
-              <div className="col-8 text-center">
-                <h1>GALERIES</h1>
+          <div className="row justify-content-center mt-5 mb-5">
+            <div className="col-8 text-center">
+              <h1>GALERIES</h1>
+            </div>
+          </div>
+          <div className="row justify-content-center mx-auto mb-5">
+            <div className="col-md-10 text-center">
+              <Gallery
+                images={images}
+                thumbnailStyle={this.styleSmall}
+                enableImageSelection={false}
+              />
+            </div>
+          </div>
+        </div>
+        <div
+          style={{
+            backgroundColor: "#277189",
+            height: "100px",
+            marginTop: "100px",
+          }}
+        >
+          <div className="container">
+            <div className="row justify-content-center text-center">
+              <div className="col-8 dimobile" style={{ marginTop: "22px" }}>
+                <h3
+                  className="mr-3"
+                  style={{ color: "white", display: "inline" }}
+                >
+                  <i className="fa fa-youtube-play" aria-hidden="true" />
+                </h3>
+                <h3
+                  className="mr-3"
+                  style={{ color: "white", display: "inline" }}
+                >
+                  <i class="fa fa-instagram" aria-hidden="true"></i>
+                </h3>
+                <h3
+                  className="mr-3"
+                  style={{ color: "white", display: "inline" }}
+                >
+                  <i class="fa fa-twitter" aria-hidden="true"></i>
+                </h3>
               </div>
             </div>
-            <div className="row justify-content-center mx-auto mb-5">
-              <div className="col-md-10 text-center">
-                <Gallery
-                  images={images}
-                  thumbnailStyle={this.styleSmall}
-                  enableImageSelection={false}
-                />
-              </div>
+            <div className="row justify-content-center text-center mt-2">
+              <h6
+                className="email"
+                style={{ color: "white", display: "inline" }}
+              >
+                baginda13srg@gmail.com
+              </h6>
             </div>
-          </Fade>
+          </div>
         </div>
       </Fragment>
     );
