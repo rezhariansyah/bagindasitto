10b184b6920a95e6658f61cbad07b48ffaa51fa7
rezhariansyah <rezharatulangi@gmail.com>
2020-04-14 18:24:50 +0800

    underconstruction

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/header/Header";
 import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./screens/home/Home";
 import Phinisi from "./screens/phinisi/Phinisi";
+import UnderConstruction from "./screens/underConstruction/UnderConstruction";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Route path="/" component={Home} exact />
       <Route path="/phinisi" component={Phinisi} exact />
+      <Route path="/under-construction" component={UnderConstruction} exact />
     </BrowserRouter>
   );
 }
