2dc1bc029281c93d3784b1da65cc47b6b2a4becb
rezhariansyah <rezharatulangi@gmail.com>
2020-04-16 03:22:31 +0800

    add react helmet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import { Helmet } from "react-helmet";
+import Favicon from "./assets/banners/icon/favicon.ico"
 import Header from "./components/header/Header";
 import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./screens/home/Home";
@@ -11,15 +13,25 @@ import Contact from "./screens/contact/Contact";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Route path="/" component={Home} exact />
-      <Route path="/phinisi" component={Phinisi} exact />
-      <Route path="/about" component={About} exact />
-      <Route path="/tips" component={Tips} exact />
-      <Route path="/travel" component={Travel} exact />
-      <Route path="/contact" component={Contact} exact />
-    </BrowserRouter>
+    <div>
+    <Helmet>
+      <meta charSet="UTF-8" />
+      <title>Baginda Sitto</title>
+      <meta name="description" content="Hello, as a Bira based Architects, Boat Consultant, and Content Creator. Sitto and team has completed several boat design and construction project - Producing Eco Friendly Boat, Out of Box design, and Boat according to cruise safety standards. We're also your travelmate,
+      let's go on an adventure" />
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+      <link rel="shortcut icon" href={Favicon} />
+    </Helmet>
+      <BrowserRouter>
+        <Header />
+        <Route path="/" component={Home} exact />
+        <Route path="/phinisi" component={Phinisi} exact />
+        <Route path="/about" component={About} exact />
+        <Route path="/tips" component={Tips} exact />
+        <Route path="/travel" component={Travel} exact />
+        <Route path="/contact" component={Contact} exact />
+      </BrowserRouter>
+    </div>
   );
 }
 
